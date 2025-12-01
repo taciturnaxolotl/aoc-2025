fbfecad0ba78eaa0f6ae99bd9ea8665e5d5b5c7e
Kieran Klukas <me@dunkirk.sh>
2025-11-30 22:25:41 -0800

    feat: add part two of day 1

--- ts/01/index.ts
+++ ts/01/index.ts
@@ -1,18 +1,53 @@
 const file = await Bun.file("./input.txt").text();
 
-let dial = 50;
-let count = 0;
+(() => {
+	let dial = 50;
+	let count = 0;
 
-file.split("\n").forEach((line) => {
-	const dir = line.startsWith("R"); // false is left
-	const num = Number.parseInt(line.substring(1), 10);
+	file.split("\n").forEach((line) => {
+		const dir = line.startsWith("R"); // false is left
+		const num = Number.parseInt(line.substring(1), 10);
 
-	if (dir) dial -= num;
-	else dial += num;
+		if (dir) dial += num;
+		else dial -= num;
 
-	dial %= 100;
+		dial %= 100;
 
-	if (dial === 0) count++;
-});
+		if (dial === 0) count++;
+	});
 
-console.log(`part 1: ${count}`);
+	console.log(`part 1: ${count}`);
+})();
+
+(() => {
+	let dial = 50;
+	let count = 0;
+
+	file.split("\n").forEach((line) => {
+		line = line.trim();
+		const dir = line[0]; // false is left
+		const num = Number.parseInt(line.substring(1), 10);
+
+		const dialBefore = dial;
+
+		let distToZero: number;
+		if (dir === "R") {
+			distToZero = (100 - dialBefore) % 100;
+		} else {
+			distToZero = dialBefore % 100;
+		}
+
+		if (distToZero === 0) distToZero = 100;
+
+		if (num >= distToZero) {
+			count += 1 + Math.floor((num - distToZero) / 100);
+		}
+
+		if (dir === "R") dial = (dialBefore + num) % 100;
+		else dial = (100 + dialBefore - (num % 100)) % 100;
+
+		dial = (100 + dial) % 100;
+	});
+
+	console.log(`part 2: ${count}`);
+})();
